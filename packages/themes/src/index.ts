95004fa64086eaa71266660723d81f510573559b
Ghazi <hello@mgks.dev>
2026-03-11 22:17:42 +0530

    Migrate @docmd/themes to typescript

--- packages/themes/src/index.ts
+++ packages/themes/src/index.ts
@@ -6,13 +6,17 @@
  * @website     https://docmd.io
  * @repository  https://github.com/docmd-io/docmd
  * @license     MIT
- * @copyright   Copyright (c) 2025 docmd.io
+ * @copyright   Copyright (c) 2025-present docmd.io
  *
  * [docmd-source] - Please do not remove this header.
  * --------------------------------------------------------------------
  */
 
-const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * Returns the absolute path to the requested theme CSS file.
@@ -20,20 +24,20 @@ const path = require('path');
  * @returns {string} Absolute path to css file
  */
 
-function getThemePath(themeName) {
+function getThemePath(themeName: string) {
   const cleanName = themeName.toLowerCase();
-  // We point to src for now, or dist if you plan to add a build step later
-  return path.join(__dirname, 'src', `${cleanName}.css`);
+  // Using path.join(__dirname, '..', 'src') because this file runs from dist/
+  return path.join(__dirname, '..', 'src', `docmd-theme-${cleanName}.css`);
 }
 
 /**
  * Returns the directory containing all themes
  */
 function getThemesDir() {
-  return path.join(__dirname, 'src');
+  return path.join(__dirname, '..', 'src');
 }
 
-module.exports = {
+export {
   getThemePath,
   getThemesDir
 };
